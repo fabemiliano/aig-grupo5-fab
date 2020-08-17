f7d6bbd3f6daf6ffc1895f175c6aaecadef049d9
fabemiliano <fab.emiliano@gmail.com>
2020-08-17 14:33:43 -0300

    Updates

---
+++ precache-manifest.4079356b47a778a1ba863f042aa53060.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894bf3565c98d399e1e71eaef586144c",
+    "url": "/aig-grupo5-fab/index.html"
+  },
+  {
+    "revision": "df38f0773addc812e1dc",
+    "url": "/aig-grupo5-fab/static/css/main.35a1d72c.chunk.css"
+  },
+  {
+    "revision": "e6172c570ca53cdf8d97",
+    "url": "/aig-grupo5-fab/static/js/2.26277251.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/aig-grupo5-fab/static/js/2.26277251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df38f0773addc812e1dc",
+    "url": "/aig-grupo5-fab/static/js/main.e353012d.chunk.js"
+  },
+  {
+    "revision": "d07ed99e4b502690fb88",
+    "url": "/aig-grupo5-fab/static/js/runtime-main.7aeb468b.js"
+  },
+  {
+    "revision": "c57ede1de60f117e50b6d0cb5e55240c",
+    "url": "/aig-grupo5-fab/static/media/Roboto-LightItalic.c57ede1d.ttf"
+  },
+  {
+    "revision": "7e4af5d0d573f2212a9ae589695d36ff",
+    "url": "/aig-grupo5-fab/static/media/biglogo.7e4af5d0.png"
+  },
+  {
+    "revision": "51615cc1b652851cc46fc347b89b9597",
+    "url": "/aig-grupo5-fab/static/media/bottles.51615cc1.svg"
+  },
+  {
+    "revision": "a240077cc4923fd39bf41d87e7833083",
+    "url": "/aig-grupo5-fab/static/media/cart.a240077c.svg"
+  },
+  {
+    "revision": "3d733ec5feea4581255a7f2ebeff96a3",
+    "url": "/aig-grupo5-fab/static/media/logo.3d733ec5.svg"
+  },
+  {
+    "revision": "b550c33e605234ba9aaea5184961b29f",
+    "url": "/aig-grupo5-fab/static/media/rubish.b550c33e.svg"
+  },
+  {
+    "revision": "a75c4d09a8e5bd9554294c91b17f3ef0",
+    "url": "/aig-grupo5-fab/static/media/tela1R.a75c4d09.png"
+  },
+  {
+    "revision": "204ec3502247909211313c9acfbd8e6d",
+    "url": "/aig-grupo5-fab/static/media/tela2R.204ec350.png"
+  },
+  {
+    "revision": "228cd08f4aab4f79095b20c6acf9e040",
+    "url": "/aig-grupo5-fab/static/media/user.228cd08f.svg"
+  }
+]);
